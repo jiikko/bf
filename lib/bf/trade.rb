7fc9281e909aeb8fa7b3f2853180a70abb178306
jiikko <n905i.1214@gmail.com>
2018-03-25 20:45:27 +0900

    最終取引価格を取得してくる

--- lib/bf/trade.rb
+++ lib/bf/trade.rb
@@ -12,13 +12,14 @@ module BF
     enum kind: RANGE_CONST.keys
 
     def self.fetch
+      create!(kind: :minutely, price: BF::Client.get_ticker['ltp'])
     end
 
     def self.minutely_range
       from = RANGE_CONST[:minutely].ago
       to = Time.now
-      BF::Trade.where(kind: :minutely,
-                      created_at: (from..to)).pluck('max(price), min(price)').to_a.first || [0, 0]
+      where(kind: :minutely,
+            created_at: (from..to)).pluck('max(price), min(price)').to_a.first || [0, 0]
     end
   end
 end
--- spec/bf/trade_spec.rb
+++ spec/bf/trade_spec.rb
@@ -1,8 +1,17 @@
 require 'spec_helper'
 
 RSpec.describe BF::Trade do
-  describe 'minutely_range' do
+  describe '.fetch' do
+    it '' do
+      BF::Trade.delete_all
+      BF::Trade.fetch
+      expect(BF::Trade.count).to eq(1)
+    end
+  end
+
+  describe '.minutely_range' do
     it do
+      BF::Trade.delete_all
       BF::Trade.create!(price: 1200, kind: :minutely, created_at: 2.minute.ago)
       BF::Trade.create!(price: 1100, kind: :hourly)
       BF::Trade.create!(price: 1000, kind: :minutely)
