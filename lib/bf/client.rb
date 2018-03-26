1a054e699fc8b9f35acace03d7c1259fc60c1423
jiikko <n905i.1214@gmail.com>
2018-03-26 20:10:37 +0900

    Add Exception class for retry

--- lib/bf/client.rb
+++ lib/bf/client.rb
@@ -21,7 +21,7 @@ module BF
       body = nil
       begin
         body = https.start { |https| response = https.get(pt) }.body
-      rescue Net::HTTPBadResponse, Errno::ECONNRESET => e
+      rescue Net::HTTPBadResponse, Errno::ECONNRESET,  Errno::EHOSTUNREACH => e
         retry
       end
       JSON.parse(body)
