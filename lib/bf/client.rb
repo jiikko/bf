570bba4b102dbe4334738eda2cc8baa886a5462f
yamada kawaguchi <jiikko@users.noreply.github.com>
2018-03-30 23:09:57 +0900

    Update client.rb

--- lib/bf/client.rb
+++ lib/bf/client.rb
@@ -21,7 +21,7 @@ module BF
       body = nil
       begin
         body = https.start { |https| response = https.get(pt) }.body
-      rescue Net::HTTPBadResponse, Errno::ECONNRESET,  Errno::EHOSTUNREACH => e
+      rescue OpenSSL::SSL::SSLError, Net::HTTPBadResponse, Errno::ECONNRESET,  Errno::EHOSTUNREACH => e
         retry
       end
       JSON.parse(body)
