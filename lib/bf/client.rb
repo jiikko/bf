0ad811e7661891e8bdbd6c2ee5a28d741bf78ca6
jiikko <n905i.1214@gmail.com>
2018-04-02 00:39:25 +0900

    買い注文をできるようにする

--- lib/bf/client.rb
+++ lib/bf/client.rb
@@ -1,4 +1,5 @@
 require 'net/https'
+require "openssl"
 require 'json'
 
 module BF
@@ -12,6 +13,16 @@ module BF
       def get_ticker
         new.get("/v1/ticker", PROCUT_CODE)
       end
+    end
+
+    def initialize(path: nil)
+      path =
+        if defined?(Rails)
+          path = File.join(Rails.root, 'bf_config.yaml')
+        else
+          path ||= 'bf_config.yaml'
+        end
+      @config = YAML.load(File.open(path))
     end
 
     def get(path, product_code)
@@ -30,13 +41,41 @@ module BF
       JSON.parse(body)
     end
 
-    def buy(price)
+    def buy(price, size)
+      default_body = {
+        product_code: PROCUT_CODE,
+        child_order_type: 'LIMIT',
+        side: 'BUY',
+      }
+      body = default_body.merge(price: price, size: size).to_json
+      method = 'POST'
+      timestamp = Time.now.to_i.to_s
+      uri = URI.parse("https://#{END_POINT}")
+      uri.path = '/v1/me/sendchildorder'
+      text = [timestamp, method, uri.request_uri, body].join
+      sign = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("sha256"), api_secret, text)
+      options = Net::HTTP::Post.new(uri.request_uri, initheader = {
+        "ACCESS-KEY" => api_key,
+        "ACCESS-TIMESTAMP" => timestamp,
+        "ACCESS-SIGN" => sign,
+        "Content-Type" => "application/json",
+      })
+      options.body = body
+      https = Net::HTTP.new(uri.host, uri.port)
+      https.use_ssl = true
+      response = https.request(options)
+      puts response.body
     end
 
     def sell(price)
     end
 
-    def min_price_by_current_range
+    def api_key
+      @config['api_key']
+    end
+
+    def api_secret
+      @config['api_secret']
     end
   end
 end
