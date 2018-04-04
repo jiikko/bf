6712e2cb79d8f43821d0bb72c7bfd3e42af4286a
jiikko <n905i.1214@gmail.com>
2018-04-04 23:41:24 +0900

    注文のメソッドを共通クラスに移動する

--- lib/bf/client.rb
+++ lib/bf/client.rb
@@ -5,6 +5,70 @@ require 'json'
 module BF
   # https://lightning.bitflyer.jp/docs?lang=ja#http-public-api
   class Client
+    class BaseRequest
+      def  initialize(path: nil)
+        path =
+          if defined?(Rails)
+            File.join(Rails.root, 'bf_config.yaml')
+          else
+            'bf_config.yaml'
+          end
+        @config = YAML.load(File.open(path))
+      end
+
+      def api_key
+        @config['api_key']
+      end
+
+      def api_secret
+        @config['api_secret']
+      end
+
+      def timestamp
+        @timestamp ||= Time.now.to_i.to_s
+      end
+
+      def http_method
+        :POST
+      end
+
+      def uri
+        @uri ||= URI.parse("https://#{END_POINT}")
+      end
+
+      def run(path: , http_class: )
+        body = yield
+        uri.path = path
+        text = [timestamp, http_method, uri.request_uri, body].join
+        sign = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("sha256"), api_secret, text)
+        options = Net::HTTP::Post.new(uri.request_uri, initheader = {
+          "ACCESS-KEY" => api_key,
+          "ACCESS-TIMESTAMP" => timestamp,
+          "ACCESS-SIGN" => sign,
+          "Content-Type" => "application/json",
+        })
+        options.body = body
+        https = http_class.new(uri.host, uri.port)
+        https.use_ssl = true
+        response = https.request(options)
+        BF.logger.info response.body
+        JSON.parse(response.body)['child_order_acceptance_id']
+      end
+    end
+
+    class BuyRequest < BaseRequest
+      def run(price, size)
+        response = super(path: '/v1/me/sendchildorder', http_class: Net::HTTP) do
+          default_body = {
+            product_code: PROCUT_CODE,
+            child_order_type: 'LIMIT',
+            side: 'BUY',
+          }
+          default_body.merge(price: price, size: size).to_json
+        end
+      end
+    end
+
     class << self
       def get_state
         new.get("/v1/getboardstate", PROCUT_CODE)
@@ -15,14 +79,7 @@ module BF
       end
     end
 
-    def initialize(path: nil)
-      path =
-        if defined?(Rails)
-          path = File.join(Rails.root, 'bf_config.yaml')
-        else
-          path ||= 'bf_config.yaml'
-        end
-      @config = YAML.load(File.open(path))
+    def initialize
     end
 
     def get(path, product_code)
@@ -42,40 +99,10 @@ module BF
     end
 
     def buy(price, size)
-      default_body = {
-        product_code: PROCUT_CODE,
-        child_order_type: 'LIMIT',
-        side: 'BUY',
-      }
-      body = default_body.merge(price: price, size: size).to_json
-      method = 'POST'
-      timestamp = Time.now.to_i.to_s
-      uri = URI.parse("https://#{END_POINT}")
-      uri.path = '/v1/me/sendchildorder'
-      text = [timestamp, method, uri.request_uri, body].join
-      sign = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("sha256"), api_secret, text)
-      options = Net::HTTP::Post.new(uri.request_uri, initheader = {
-        "ACCESS-KEY" => api_key,
-        "ACCESS-TIMESTAMP" => timestamp,
-        "ACCESS-SIGN" => sign,
-        "Content-Type" => "application/json",
-      })
-      options.body = body
-      https = Net::HTTP.new(uri.host, uri.port)
-      https.use_ssl = true
-      response = https.request(options)
-      puts response.body
+      BuyRequest.new.run(price, size)
     end
 
     def sell(price)
-    end
-
-    def api_key
-      @config['api_key']
-    end
-
-    def api_secret
-      @config['api_secret']
     end
   end
 end
