39ed108d0b9e074e12dde8a167beee9a3f237844
jiikko <n905i.1214@gmail.com>
2018-05-16 05:55:30 +0900

    add spec

--- spec/bf/scalping_task_spec.rb
+++ spec/bf/scalping_task_spec.rb
@@ -1,39 +1,49 @@
 require 'spec_helper'
 
 RSpec.describe BF::ScalpingTask do
-  before do
+  before(:each) do
+    BF::MyTradeShip.delete_all
     BF::MyTrade.delete_all
     BF::ScalpingTask.delete_all
     ResqueSpec.reset!
+    allow_any_instance_of(BF::Client).to receive(:buy).and_return(1)
   end
 
   describe '.running?' do
     context '実行中ステータスのtaskがある時' do
       context '注文した直後の時' do
         it 'return true' do
-          allow_any_instance_of(BF::Client).to receive(:buy).and_return(1)
           buy_trade = BF::MyTrade.new.run_buy_trade!(10)
           BF::ScalpingTask.create!(trade_ship_id: buy_trade.trade_ship.id)
           expect(BF::ScalpingTask.running?).to eq(true)
         end
       end
     end
-    context '買いが約定した直後の時' do
+    context '買いが約定した直後(売り約定待ち)の時' do
       it 'return true' do
-        pending
+        buy_trade = BF::MyTrade.new.run_buy_trade!(10)
+        BF::ScalpingTask.create!(trade_ship_id: buy_trade.trade_ship.id)
+        buy_trade.succeed!
+        buy_trade.trade_ship.sell_trade.waiting_to_sell!
+        expect(BF::ScalpingTask.running?).to eq(true)
       end
     end
 
     context '実行中ステータスのtaskがない時' do
       context '売りが約定した直後の時' do
         it 'return false' do
-        pending
+          buy_trade = BF::MyTrade.new.run_buy_trade!(10)
+          BF::ScalpingTask.create!(trade_ship_id: buy_trade.trade_ship.id)
+          buy_trade.succeed!
+          buy_trade.trade_ship.sell_trade.succeed!
+          expect(BF::ScalpingTask.running?).to eq(false)
         end
       end
+    end
+
+    context 'BF::ScalpingTaskレコードが無い時' do
       it 'return false' do
-        BF::MyTrade.delete_all
-        BF::MyTradeShip.delete_all
-        BF::ScalpingTask.delete_all
+        expect(BF::ScalpingTask.count).to eq(0)
         expect(BF::ScalpingTask.running?).to eq(false)
       end
     end
