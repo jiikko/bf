61c62813e1eb4c5c096dab2403b6be65f033df79
jiikko <n905i.1214@gmail.com>
2018-05-20 12:55:58 +0900

    Impliment MyTradeShip#running?

--- lib/bf/my_trade_ship.rb
+++ lib/bf/my_trade_ship.rb
@@ -4,5 +4,9 @@ module BF
     belongs_to :sell_trade, class_name: 'BF::MyTrade'
 
     has_one :scalping_task, foreign_key: :trade_ship_id
+
+    def running?
+      buy_trade.running? && sell_trade.running?
+    end
   end
 end
