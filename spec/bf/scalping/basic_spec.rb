fd3e297c1c07c4f06ed0f6b62950ea52796f6c35
jiikko <n905i.1214@gmail.com>
2018-05-26 23:24:57 +0900

    add makiami test

---
+++ spec/bf/scalping/basic_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe BF::Scalping::Basic do
+  describe '.scalp' do
+    context 'vaild? is true' do
+      it 'ScalpingTaskを作成すること' do
+        scalping = BF::Scalping::Basic.new
+        allow(scalping).to receive(:valid?).and_return(true)
+        allow_any_instance_of(BF::Client).to receive(:buy).and_return(1)
+        trade = BF::Trade.create!(price: 3, kind: :minutely)
+        scalping.scalp
+        expect(BF::MyTradeShip.count).to eq(1)
+        expect(BF::ScalpingTask.count).to eq(1)
+        expect(BF::MyTradeShip.first.scalping_task).to be_a(BF::ScalpingTask)
+      end
+    end
+  end
+end
---
+++ spec/bf/scalping/makiami_spec.rb
@@ -0,0 +1,18 @@
+RSpec.describe BF::Scalping::Makiami do
+  describe '.scalp' do
+    context 'vaild? is true' do
+      it 'ScalpingTaskを作成すること' do
+        scalping = BF::Scalping::Makiami.new
+        allow(scalping).to receive(:valid?).and_return(true)
+        allow_any_instance_of(BF::Client).to receive(:buy).and_return(1)
+        trade = BF::Trade.create!(price: 3, kind: :minutely)
+        scalping.scalp
+        expect(BF::MyTradeShip.count).to eq(4)
+        expect(BF::ScalpingTask.count).to eq(4)
+        expect(BF::MyTradeShip.first.scalping_task).to be_a(BF::ScalpingTask)
+        expect(BF::MyTradeShip.all.map(&:buy_trade).map(&:price)).to eq([3, -497, -997, -1497])
+      end
+    end
+  end
+end
+
