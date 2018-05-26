975f2e4ed5d9dc7d71b48b708dbd2ab1b404faac
jiikko <n905i.1214@gmail.com>
2018-05-26 12:03:19 +0900

    more exception log

--- lib/bf/worker/base_worker.rb
+++ lib/bf/worker/base_worker.rb
@@ -15,7 +15,7 @@ module BF
         BF.logger.info "#{self.inspect}(#{args}) をqueueに積み直しました"
         self.perform_async(*args)
       else
-        BF.logger.info "#{self.inspect}(#{args}) で想定外の例外を検出しました。何もしていません。"
+        BF.logger.info "[#{e.inspect}] #{self.inspect}(#{args}) で想定外の例外を検出しました。何もしていません。"
         BF.logger.info e.message
       end
     end
