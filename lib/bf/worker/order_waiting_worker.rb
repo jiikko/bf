585e368977ca519900cf9ec9016039acc3ee5a64
jiikko <n905i.1214@gmail.com>
2018-06-03 16:47:32 +0900

    安定向上

--- lib/bf/worker/order_waiting_worker.rb
+++ lib/bf/worker/order_waiting_worker.rb
@@ -3,6 +3,7 @@ module BF
     def perform(sell_trade_id)
       sell_trade = BF::MyTrade.find_by_sell(sell_trade_id)
       loop do
+        sell_trade.reload
         if sell_trade.trade_sccessd?
           sell_trade.succeed!
           BF.logger.info '売りを確認しました。'
