1b4ff6bcfec14f261a3a64bf715766d4cf3256ec
jiikko <n905i.1214@gmail.com>
2018-06-30 08:19:28 +0900

    日付の範囲条件が間違っているのを修正する

--- lib/bf/summarized_my_trade.rb
+++ lib/bf/summarized_my_trade.rb
@@ -3,9 +3,12 @@ module BF
     enum kind: [:manual, :automation]
 
     def self.summarize!(ago_days: 2.day)
+      if ago_days.is_a?(Integer)
+        ago_days = ago_days.day
+      end
       # TODO SQLでやる
       ships_table = BF::MyTradeShip.succeed.includes(:scalping_task, :buy_trade, :sell_trade).
-        where("#{BF::MyTradeShip.table_name}.created_at > ?", ago_days.ago.beginning_of_day).order(created_at: :desc).
+        where("#{BF::MyTradeShip.table_name}.created_at > ?", (Time.now.beginning_of_day - ago_days)).order(created_at: :desc).
         group_by { |x| x.created_at.localtime.strftime('%Y/%m/%d') }
 
       ships_table.each do |date, tasks|
