b1405025f433dd71b93f9dc3ca8800000b55bf42
jiikko <n905i.1214@gmail.com>
2018-07-07 21:46:02 +0900

    add spec

--- spec/bf/summarized_my_trade_spec.rb
+++ spec/bf/summarized_my_trade_spec.rb
@@ -8,21 +8,24 @@ RSpec.describe BF::SummarizedMyTrade do
       Factory.create_my_trade_ship(created_at: today)
       Factory.create_my_trade_ship(created_at: prev_day)
       Factory.create_my_trade_ship(created_at: prev_day)
-      Factory.create_my_trade_ship(created_at: prev_day, by: :automation)
+      Factory.create_my_trade_ship(created_at: prev_day, by: :manual)
+      BF::SummarizedMyTrade.summarize!
 
       prev_day_relation = BF::SummarizedMyTrade.where(summarized_on: prev_day.strftime('%Y/%m/%d'))
       today_relation = BF::SummarizedMyTrade.where(summarized_on: today.strftime('%Y/%m/%d'))
       expect(BF::SummarizedMyTrade.count).to eq(4)
-      expect(today_relation.count).cout).to eq(1)
-      expect(prev_day_relation.count).cout).to eq(3)
-      prev_day_relation_table_by_kind = prev_day_relation.group_by(:kind)
-      expect(prev_day_relation_table_by_kind[:manual].size).to eq(2)
-      expect(prev_day_relation_table_by_kind[:automation].size).to eq(1)
+      expect(today_relation.count).to eq(2)
+      expect(prev_day_relation.count).to eq(2)
+      manual_prev_day_relation = prev_day_relation.find { |x| x.kind == "manual" }
+      expect(manual_prev_day_relation.count).to eq(1)
+      expect(manual_prev_day_relation.profit).to eq(10)
+      automation_prev_day_relation = prev_day_relation.find { |x| x.kind == "automation" }
+      expect(automation_prev_day_relation.count).to eq(2)
+      expect(automation_prev_day_relation.profit).to eq(20)
     end
 
     context 'when has args' do
       it 'be success' do
-        BF::MyTradeShip.create
       end
     end
   end
--- spec/support/factory.rb
+++ spec/support/factory.rb
@@ -1,9 +1,9 @@
 class Factory
   class << self
     def create_my_trade_ship(created_at: nil, by: :automation)
-      sell_trade = BF::MyTrade.create!(price: 1, size: 0.1, status: :succeed, kind: :sell)
-      buy_trade  = BF::MyTrade.create!(price: 1, size: 0.1, status: :succeed, kind: :buy)
-      running_ship = BF::MyTradeShip.create!(sell_trade: sell_trade, buy_trade: buy_trade, created_at: created_at)
+      sell_trade = BF::MyTrade.create!(price: 100, size: 0.1, status: :succeed, kind: :sell)
+      buy_trade  = BF::MyTrade.create!(price: 0, size: 0.1, status: :succeed, kind: :buy)
+      running_ship = BF::MyTradeShip.create!(sell_trade_id: sell_trade.id, buy_trade_id: buy_trade.id, created_at: created_at)
       case by
       when :automation
         running_ship.create_scalping_task
